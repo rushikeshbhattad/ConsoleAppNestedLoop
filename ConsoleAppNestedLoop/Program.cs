3c85fdd36f7c6cb25caec56c8aa21609cff76d31
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 15:32:42 +0530

    Add project files.

---
+++ ConsoleAppNestedLoop/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace DemoNestedLoop
+{
+    class Pattern1
+    {
+        public void Pat1(int num)
+        {
+            int i, j;
+            int number;
+            for (i = 0; i < num; i++)
+            {
+                number = 1;
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write(number);
+                    number++;
+                }
+                Console.WriteLine(" ");
+
+            }
+        }
+        public void Pat2(int num)
+        {
+            int i, j;
+            int number;
+            for (i = 0; i < num; i++)
+            {
+                number = 5;
+                for (j = 0; j <= i; j++)
+                {
+                    Console.Write(number);
+                    number--;
+                }
+                Console.WriteLine(" ");
+
+            }
+        }
+        public void Pat3(int num)
+        {
+            int i, j;
+            int number;
+            for (i = 0; i <=num; i++)
+            {
+                number = 5;
+                for (j = 0; j < i; j++)
+                {
+                    Console.Write(number);
+                    number--;
+                }
+                Console.WriteLine(" ");
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern1 p = new Pattern1();
+            p.Pat3(5);
+            Console.ReadKey();
+
+        }
+    }
+}
